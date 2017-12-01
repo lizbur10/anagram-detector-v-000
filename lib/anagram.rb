bfabfc70ee9b896bce65537ca94a8b1243178149
Liz Burton <liz.burton147@gmail.com>
2017-12-01 14:20:34 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+
+  def initialize(string)
+    @string = string
+  end
+
+  def match(word_array)
+    return_array = []
+    word_array.each do | word |
+      if word.chars.sort == @string.chars.sort
+        return_array << word
+      end
+    end
+    return_array
+  end
+
+end
